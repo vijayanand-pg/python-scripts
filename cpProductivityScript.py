3a697263a31990c803288b61d1c34b82c3f8577f
Vijayanand Gajapathy <vijayanand.pg6@gmail.com>
2019-12-26 01:25:14 +0530

    Update cpProductivityScript.py

--- cpProductivityScript.py
+++ cpProductivityScript.py
@@ -3,6 +3,7 @@
 
 import win32clipboard
 import subprocess
+from datetime import date
 
 filePath = "C:\\"
 win32clipboard.OpenClipboard()
@@ -20,6 +21,8 @@ f = open(filePath + fileName, "w+")
 f.write("# python file created using cpProductivityScript.py")
 f.write("# https://github.com/vijayanand-pg/python-scripts/")
 f.write("\n\n\n\n\n# --signature")
+currentDate = date.today().strftime("%b-%d-%Y")
+f.write("\n# --" + currentDate)
 f.close()
 
 p = subprocess.Popen(["code.exe", filePath + fileName])
